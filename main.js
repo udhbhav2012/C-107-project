8f4968b7d098200fca9e8346dc5abe6882119d48
udhbhav2012 <82303525+udhbhav2012@users.noreply.github.com>
2021-08-20 15:59:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/1XKMUZUTT/model.json', modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results)
+{
+    console.log(gotResults)
+    
+}
